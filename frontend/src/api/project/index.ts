996ebb1bb9986c6e4bceab56cfee35a057e3bbff
Jeremy_D <1418776145@qq.com>
2026-07-29 14:41:12 +0800

    refactor: remove contract & env files, replace with internal model types

--- frontend/src/api/project/index.ts
+++ frontend/src/api/project/index.ts
@@ -1,30 +1,4 @@
 export { useCreateProjectMutation } from "./project-create.mutation";
-export {
-  toCreateProjectRequest,
-  toProjectSummary as toProjectSummaryFromDto,
-  toUpdateProjectRequest,
-} from "./project.mapper";
-export {
-  createProjectBackendApi,
-  projectEndpoints,
-  type CreateProjectRequest,
-  type CreateProjectResponse,
-  type DeleteProjectRequest,
-  type DeleteProjectResponse,
-  type ListProjectsRequest,
-  type ListProjectsResponse,
-  type ProjectBackendApi,
-  type ProjectDetailRequest,
-  type ProjectDetailResponse,
-  type ProjectDto,
-  type ProjectGameType,
-  type ProjectPlatformType,
-  type ProjectRequestExecutor,
-  type ProjectRequestInput,
-  type ProjectViewType,
-  type UpdateProjectRequest,
-  type UpdateProjectResponse,
-} from "./project.contract";
 export { useDeleteProjectMutation } from "./project-delete.mutation";
 export { useProjectListQuery } from "./project-list.query";
 export {
